1fd6f3c8fa42db2ee596e5cd62b3bcaef1c312a4
Alessio Coppola <alessio.coppola@capgemini.com>
2023-04-12 12:15:47 +0200

    review tests

--- tests.py
+++ tests.py
@@ -1,22 +1,21 @@
 # tests.py
 
-import random
-try:
-    import unittest2 as unittest
-except ImportError:
-    import unittest
+import xmlrunner
+import unittest
 
 class SimpleTest(unittest.TestCase):
-    @unittest.skip("demonstrating skipping")
-    def test_skipped(self):
-        self.fail("shouldn't happen")
-
-    def test_pass(self):
+    
+    def test_pass1(self):
         self.assertEqual(10, 7 + 3)
 
-    def test_fail(self):
+    def test_pass2(self):
+        self.assertTrue(True)
+
+    def test_fail1(self):
         self.assertEqual(11, 7 + 3)
 
+    def test_fail1(self):
+        self.assertFalse(True)
+
 if __name__ == '__main__':
-    import xmlrunner
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
